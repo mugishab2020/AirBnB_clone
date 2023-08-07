5f205034cb5297820e5f7a95a14b10ace5ce62f8
mugishab2020 <mugishab2020@gmail.com>
2023-08-07 14:17:15 -0700

    Trying

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" making the model class as base class
+"""
+
+from uuid import uuid4
+from datetime import date, datetime
+import models
+
+class BaseModel:
+    def __init__(self, *args, **kwagrs):
+         if len(kwargs) != 0:
+             for key, value in kwargs.item():
+                 if key == "__clas__":
+                     continue
+                 elif key == "created_at" or key == "updated_at":
+                     setattr(self, key, datetime.fromisoformat(value))
+                 else:
+                     setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = created_at
+    def __str__(self):
+        """ should print: [<class name>] (<self.id>) <self.__dict__>"""
+        name = self.__class__.__name__
+        return "[{} ({}) ()".format(name, self.id, self.__dict__)
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        new_dict = self.__dict__.copy()
+        # Add __class__ key to dictionary
+        new_dict['__class__'] = self.__class__.__name__
+        # Convert created_at and updated_at to ISO format strings
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
